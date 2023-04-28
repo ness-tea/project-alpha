eb8f102e6f696908cdb98cd2c2e321328f1e29cc
jaskiewm <m.jasski@hotmail.com>
2023-04-27 23:53:10 -0400

    Added Al Program Theme + Al Layout
    - Added custom theme for Al Program organization
    - Added aesthetics for background of Al program
    - Added timeline start to Al Program (temp for testing)

--- scripts/al.gd
+++ scripts/al.gd
@@ -8,9 +8,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var alDialog1 = Dialogic.start("FirstEncounterWithAl")
+	add_child(alDialog1)
+	alDialog1.connect("timeline_end", self, "after_dialog")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+	
