46a89f4e67c0cf95c9818f77b34edbd2ebe05d13
Mark JD <47066420+jaskiewm@users.noreply.github.com>
2023-04-28 23:24:01 -0400

    Mark branch (#3)
    * Added Desktop App Functions
    
    - Added volume control & slider
    - Added start menu buttons
    - Added music to folder
    
    * Updated Al Portrait
    
    Changed offset and scale (temp, needs more adjusting and might need dependent sizes based on computer resolution)
    
    * Moved Files
    
    Moved Volume Control from scenes to scripts
    
    * Added Volume Messenger Icon
    
    - Added Volume Messenger Icon
    
    * Added Al Program Theme + Al Layout
    
    - Added custom theme for Al Program organization
    - Added aesthetics for background of Al program
    - Added timeline start to Al Program (temp for testing)
    
    * Updated Window Size
    
    Updated window size to 1920x1080
    
    * General Dialogic Changes
    
    - Removed "Full Width" box for default theme
    - Background Changes for dialogic
    
    ---------
    
    Co-authored-by: Vanessa Truong <v.truong9710@gmail.com>

--- scripts/VolumeControl.gd
+++ scripts/VolumeControl.gd
@@ -1,6 +1,8 @@
 extends Control
 
 
+onready var volume = $Panel/VolumeSlider
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,12 +10,16 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var alDialog1 = Dialogic.start("FirstEncounterWithAl")
-	add_child(alDialog1)
-	alDialog1.connect("timeline_end", self, "after_dialog")
+	visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-	
+
+func _on_VolumeButton_pressed():
+	visible = !visible
+
+
+func _on_VSlider_changed(vol):
+	AudioServer.set_bus_volume_db(0,vol) #0 is index #1. Since master is the first in the index, we use 0
