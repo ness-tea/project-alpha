4783c4b9db0f9e7a8bd5a33c6f1a83ecbc76ac33
jaskiewm <m.jasski@hotmail.com>
2023-05-02 00:50:03 -0400

    Updated Messenger Control
    Added more messages and updated layout & buttons of messenger

--- scripts/desktop.gd
+++ scripts/desktop.gd
@@ -1,14 +1,24 @@
 extends Control
 
+var projectAlClick = 0
+var projectAlClickTime = 0
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
+func _process(delta):
+	if projectAlClick == 1:
+		projectAlClickTime += delta
+	
+	if (projectAlClickTime > 2):
+		projectAlClick = 0
+		projectAlClickTime = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +28,9 @@ func _ready():
 
 func _on_Logout_pressed():
 	get_tree().quit()
+
+
+func _on_ProjectAlProgram_pressed():
+	projectAlClick += 1
+	if (projectAlClick == 2 && projectAlClickTime <= 2):
+		get_tree().change_scene("res://scenes/al.tscn")
