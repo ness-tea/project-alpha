5e04cebab9258da9a0d1e750f60da477d08bd6a7
jaskiewm <m.jasski@hotmail.com>
2023-05-04 22:45:12 -0400

    Project Al Icon Visibility
    Changed project Al icon to go invisible after it is selected

--- scripts/desktop.gd
+++ scripts/desktop.gd
@@ -3,10 +3,9 @@ extends Control
 var projectAlClick = 0
 var projectAlClickTime = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
 
 func _process(delta):
 	if projectAlClick == 1:
@@ -16,15 +15,6 @@ func _process(delta):
 		projectAlClick = 0
 		projectAlClickTime = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Logout_pressed():
 	get_tree().quit()
@@ -33,4 +23,5 @@ func _on_Logout_pressed():
 func _on_ProjectAlProgram_pressed():
 	projectAlClick += 1
 	if (projectAlClick == 2 && projectAlClickTime <= 2):
+		$ProjectAlProgram.visible = false
 		get_tree().change_scene("res://scenes/al.tscn")
