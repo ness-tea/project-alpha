52a64476559974cfe2f9d4adccdd6a703aff7cfe
Vanessa Truong <v.truong9710@gmail.com>
2023-07-29 03:23:50 -0400

    Cleanup + set window in focus when clicked

--- scripts/MessengerControl.gd
+++ scripts/MessengerControl.gd
@@ -1,13 +1,5 @@
 extends Control
 
-# Window Dragging functionality
-var status = "none"
-var offset = Vector2()
-var mouse_pos = Vector2()
-
-onready var title_node = get_node("Wallpaper/VBoxContainer/TitleBar/MessengerTitle")
-onready var titlebar_size = title_node.get_size()
-
 func _ready():
 	$".".visible = false
 	$Wallpaper/VBoxContainer/PanelContainer/Messages.visible = true
@@ -45,26 +37,53 @@ func _on_BackButton_pressed():
 	$Wallpaper/VBoxContainer/PanelContainer/SingleMessages.visible = false
 	$Wallpaper/VBoxContainer/PanelContainer/Messages.visible = true
 
+# Window Dragging functionality
+enum State {NONE, WINDOW_CLICKED, TITLEBAR_CLICKED, DRAGGING, RELEASED}
+
+var status = State.NONE
+var offset = Vector2()
+var mouse_pos = Vector2()
+
+onready var titlebar_node = get_node("Wallpaper/VBoxContainer/TitleBar/MessengerTitle")
+onready var titlebar_size = titlebar_node.get_size()
+onready var window_node = get_node("Wallpaper")
+onready var window_size = window_node.get_size()
 
 func _input(event):
+	# Get position of input event in global space
 	var event_pos = event.global_position
 	
+	# Check if input event is a mouse left click
 	if (event.is_action_pressed("ui_left_click")):
-		var bar_pos = title_node.get_global_position()
-		var target_rect = Rect2(bar_pos.x, bar_pos.y, titlebar_size.x, titlebar_size.y)
 		
-		if (target_rect.has_point(event_pos)):
-			status = "clicked"
-			offset = bar_pos - event_pos
-			raise()
+		# Get title bar position in global space
+		var titlebar_pos = titlebar_node.get_global_position()
 		
-	if (status == "clicked") and (event.is_class("InputEventMouseMotion")):
-		status = "dragging"
+		# Calculate title bar rect based on title bar global position and title bar size
+		var titlebar_rect = Rect2(titlebar_pos.x, titlebar_pos.y, titlebar_size.x, titlebar_size.y)
+		var window_rect = Rect2(titlebar_pos.x, titlebar_pos.y, window_size.x, window_size.y)
+
+		# Check if input event position is within window rect
+		if (window_rect.has_point(event_pos)):
+			status = State.WINDOW_CLICKED
+			
+			# Check if event position is within title bar rect
+			if (titlebar_rect.has_point(event_pos)):
+				status = State.TITLEBAR_CLICKED
+				
+				# Calculate the offset - to be used in repositioning window during dragging
+				offset = titlebar_pos - event_pos
+
+			# If any part of the window is clicked, put it into focus
+			raise()	
 		
-	if (status == "dragging"):
+	# Only set state to DRAGGING if title bar is clicked when mouse is in motion
+	if (status == State.TITLEBAR_CLICKED) and (event.is_class("InputEventMouseMotion")):
+		status = State.DRAGGING
+		
+	# Handling for changing a dragging window's position
+	if (status == State.DRAGGING):
 		if (event.get_button_mask() != BUTTON_LEFT):
-			status = "released"
+			status = State.RELEASED
 		else:
 			self.set_global_position(event_pos + offset)
-				
-			
