7a800dac369a67691d7b43669caf5d0becedf0f6
jaskiewm <m.jasski@hotmail.com>
2023-08-12 23:44:08 -0400

    Added Jamie Messages Script
    Script is used for going through all messages when communicating with Jamie

---
+++ scripts/JamieMessages.gd
@@ -0,0 +1,119 @@
+extends PanelContainer
+
+#var PlayerMessageCount: int = 0 #Used to identify what messaging stage the player is at (first message vs 3rd vs last)
+var newUserMessage: Label
+var newJamieMessage: Label
+var UM1 = "Yeah, for sure! I would love to :)"
+var UM2 = "It's alright...my manager is being terrible again"
+var UM3 = "It's just a lot of expectations.\n Maybe we can chat after work about it?"
+var UM4 = "Thanks Honey, I love you!"
+var JM1 = "How's work going?"
+var JM2 = "Oh no :( What is it this time?)"
+var JM3 = "I'd love to :) Don't worry, you got this! <3"
+var JM4 = "I love you too ^_^"
+
+#Jamie Message starts at 0. Once player clicks button, global var JamieMessage = 1
+#After the scenes change, Jamie's message shows and the gloabl var becomes 2
+
+func _ready():
+	$VBoxContainer2/ScrollContainer/SingleMessages/JMsg2.add_color_override("font_color",Color("ffffff"))
+	#has_color_override("font_color",Color("beff9e"))
+	
+	
+	if(GlobalVar.jamieMessageShown > 6):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM1
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM1
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM2
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM2
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM3
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM3
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM4
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM4
+		GlobalVar.jamieMessageShown+=1
+	elif(GlobalVar.jamieMessageShown > 4):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM1
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM1
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM2
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM2
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM3
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM3
+		GlobalVar.jamieMessageShown+=1
+	elif(GlobalVar.jamieMessageShown > 2):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM1
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM1
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM2
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM2
+		GlobalVar.jamieMessageShown+=1
+	elif(GlobalVar.jamieMessageShown > 0):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM1
+		_duplicateJamieMessage()
+		newJamieMessage.text = JM1
+		GlobalVar.jamieMessageShown+=1
+
+
+func _duplicateJamieMessage():
+	newJamieMessage = $VBoxContainer2/ScrollContainer/SingleMessages/JMsg1.duplicate(1)
+	$VBoxContainer2/ScrollContainer/SingleMessages.add_child(newJamieMessage)
+	$VBoxContainer2/Button.visible = true
+
+
+func _on_Button_pressed():
+	$VBoxContainer2/Button.visible = false
+	if(GlobalVar.jamieMessageShown == 0):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM1
+	elif(GlobalVar.jamieMessageShown == 2):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM2
+	elif(GlobalVar.jamieMessageShown == 4):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM3
+	elif(GlobalVar.jamieMessageShown == 6):
+		changeButtonText(GlobalVar.jamieMessageShown)
+		_duplicatePlayerMessage()
+		newUserMessage.text = UM4
+	GlobalVar.jamieMessageShown+=1
+
+
+func _duplicatePlayerMessage():
+		newUserMessage = $VBoxContainer2/ScrollContainer/SingleMessages/JMsg2.duplicate(1)
+		$VBoxContainer2/ScrollContainer/SingleMessages.add_child(newUserMessage)
+
+
+#function used to change the text present on the button after it is clicked
+func changeButtonText(PlayerMessageCount):
+	match (PlayerMessageCount):
+		0:
+			$VBoxContainer2/Button/Label.text = "Hello1"
+		2:
+			$VBoxContainer2/Button/Label.text = "Hello2"
+		4:
+			$VBoxContainer2/Button/Label.text = "Hello3"
+		6:
+			$VBoxContainer2/Button/Label.text = "Hello4"
