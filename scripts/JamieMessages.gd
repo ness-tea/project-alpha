5c3ff10fa028934de26ae4ff1278cde7442e782e
jaskiewm <m.jasski@hotmail.com>
2023-08-16 00:37:12 -0400

    Fixed Messenger Formatting & Functionality
    - Fixed formatting of button and messages in messenger app
    - Fixed functionality of messenger app by allowing messages to remain idle if the button is not clicked
    - Reduced redundant & repetitive code in script

--- scripts/JamieMessages.gd
+++ scripts/JamieMessages.gd
@@ -3,75 +3,52 @@ extends PanelContainer
 #var PlayerMessageCount: int = 0 #Used to identify what messaging stage the player is at (first message vs 3rd vs last)
 var newUserMessage: Label
 var newJamieMessage: Label
+
 var UM1 = "Yeah, for sure! I would love to :)"
 var UM2 = "It's alright...my manager is being terrible again"
 var UM3 = "It's just a lot of expectations.\n Maybe we can chat after work about it?"
 var UM4 = "Thanks Honey, I love you!"
 var JM1 = "How's work going?"
-var JM2 = "Oh no :( What is it this time?)"
+var JM2 = "Oh no :( What is it this time?"
 var JM3 = "I'd love to :) Don't worry, you got this! <3"
 var JM4 = "I love you too ^_^"
+var UM_Num = [UM1,UM2,UM3,UM4]
+var JM_Num = [JM1,JM2,JM3,JM4]
 
 #Jamie Message starts at 0. Once player clicks button, global var JamieMessage = 1
 #After the scenes change, Jamie's message shows and the gloabl var becomes 2
 
 func _ready():
 	$VBoxContainer2/ScrollContainer/SingleMessages/JMsg2.add_color_override("font_color",Color("ffffff"))
-	#has_color_override("font_color",Color("beff9e"))
-	
-	
-	if(GlobalVar.jamieMessageShown > 6):
-		changeButtonText(GlobalVar.jamieMessageShown)
+	match(GlobalVar.jamieMessageShown):
+		1:
+			_messageRepeater(1)
+			GlobalVar.jamieMessageShown+=1
+		2:
+			_messageRepeater(1)
+		3:
+			_messageRepeater(2)
+			GlobalVar.jamieMessageShown+=1
+		4:
+			_messageRepeater(2)
+		5:
+			_messageRepeater(3)
+			GlobalVar.jamieMessageShown+=1
+		6:
+			_messageRepeater(3)
+		7:
+			_messageRepeater(4)
+			GlobalVar.jamieMessageShown+=1
+	changeButtonText(GlobalVar.jamieMessageShown)
+
+
+#Message repeater used for generating pre-existing messages when the user interacts with the messenger tool
+func _messageRepeater(messageNum):
+	for n in range(1,messageNum+1):
 		_duplicatePlayerMessage()
-		newUserMessage.text = UM1
+		newUserMessage.text = UM_Num[n-1]
 		_duplicateJamieMessage()
-		newJamieMessage.text = JM1
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM2
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM2
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM3
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM3
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM4
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM4
-		GlobalVar.jamieMessageShown+=1
-	elif(GlobalVar.jamieMessageShown > 4):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM1
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM1
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM2
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM2
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM3
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM3
-		GlobalVar.jamieMessageShown+=1
-	elif(GlobalVar.jamieMessageShown > 2):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM1
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM1
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM2
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM2
-		GlobalVar.jamieMessageShown+=1
-	elif(GlobalVar.jamieMessageShown > 0):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM1
-		_duplicateJamieMessage()
-		newJamieMessage.text = JM1
-		GlobalVar.jamieMessageShown+=1
+		newJamieMessage.text = JM_Num[n-1]
 
 
 func _duplicateJamieMessage():
@@ -82,23 +59,24 @@ func _duplicateJamieMessage():
 
 func _on_Button_pressed():
 	$VBoxContainer2/Button.visible = false
-	if(GlobalVar.jamieMessageShown == 0):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM1
-	elif(GlobalVar.jamieMessageShown == 2):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM2
-	elif(GlobalVar.jamieMessageShown == 4):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM3
-	elif(GlobalVar.jamieMessageShown == 6):
-		changeButtonText(GlobalVar.jamieMessageShown)
-		_duplicatePlayerMessage()
-		newUserMessage.text = UM4
-	GlobalVar.jamieMessageShown+=1
+	if(GlobalVar.timelineDialogueNumber != 4):
+		if(GlobalVar.jamieMessageShown == 0):
+			changeButtonText(GlobalVar.jamieMessageShown)
+			_duplicatePlayerMessage()
+			newUserMessage.text = UM1
+		elif(GlobalVar.jamieMessageShown == 2):
+			changeButtonText(GlobalVar.jamieMessageShown)
+			_duplicatePlayerMessage()
+			newUserMessage.text = UM2
+		elif(GlobalVar.jamieMessageShown == 4):
+			changeButtonText(GlobalVar.jamieMessageShown)
+			_duplicatePlayerMessage()
+			newUserMessage.text = UM3
+		elif(GlobalVar.jamieMessageShown == 6):
+			changeButtonText(GlobalVar.jamieMessageShown)
+			_duplicatePlayerMessage()
+			newUserMessage.text = UM4
+		GlobalVar.jamieMessageShown+=1
 
 
 func _duplicatePlayerMessage():
@@ -111,9 +89,9 @@ func changeButtonText(PlayerMessageCount):
 	match (PlayerMessageCount):
 		0:
 			$VBoxContainer2/Button/Label.text = "Hello1"
-		2:
+		1:
 			$VBoxContainer2/Button/Label.text = "Hello2"
-		4:
+		2:
 			$VBoxContainer2/Button/Label.text = "Hello3"
-		6:
+		3:
 			$VBoxContainer2/Button/Label.text = "Hello4"
