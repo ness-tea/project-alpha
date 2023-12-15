edb0b13595969f7737e4d9069161c11b0374db41
jaskiewm <m.jasski@hotmail.com>
2023-12-14 20:23:06 -0500

    Updated Login Page Display
    - Added new Display Pictures
    - Updated names shown under Display Pictures
    - Updated positioning of objects

--- scripts/login.gd
+++ scripts/login.gd
@@ -14,22 +14,26 @@ func _on_LoginButton_pressed():
 func _ready():
 	if(GlobalVar.timelineDialogueNumber == null):
 		GlobalVar.timelineDialogueNumber = 1
-	if(GlobalVar.timelineDialogueNumber == 3):
-		GlobalVar.timelineDialogueNumber = 4
-		$UserImage.visible = false
-		$FamilyImage.visible = true
+		
 	if(GlobalVar.gameEndCondition == true):
 		_end_Game()
 	else:
 		$StartPanelContainer/LoginButton.visible = true
 		$StartPanelContainer/LockedButton.visible = false
 		$EndGameCredits.visible = false
-	
+		$Name/NameLabel.text = "Sam"
+		
+	if(GlobalVar.timelineDialogueNumber == 3):
+		GlobalVar.timelineDialogueNumber = 4
+		$UserImage.visible = false
+		$AlImage.visible = true
+		$Name/NameLabel.text = "Alpha"
 
 func _end_Game():
 	$UserImage.visible = false
-	$FamilyImage.visible = false
+	$AlImage.visible = false
 	$LockedImage.visible = true
 	$StartPanelContainer/LoginButton.visible = false
 	$StartPanelContainer/LockedButton.visible = true
 	$EndGameCredits.visible = true
+	$Name/NameLabel.text = "Locked"
