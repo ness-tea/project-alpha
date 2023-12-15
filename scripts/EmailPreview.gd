57cf346069b32cac98fd3ce137fa9375b994813e
jaskiewm <m.jasski@hotmail.com>
2023-12-14 20:27:18 -0500

    Removed Ready Function
    Removed Ready() function as it wasn't used

--- scripts/EmailPreview.gd
+++ scripts/EmailPreview.gd
@@ -4,10 +4,6 @@ extends Panel
 var firstClickEE1:bool = false
 var firstClickEE2:bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func hide_email_previews():
 	for N in get_children():
 		N.visible = false
