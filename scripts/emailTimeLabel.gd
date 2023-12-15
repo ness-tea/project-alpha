fac813e0ade47c13419a33b916dd5ec32d55024b
jaskiewm <m.jasski@hotmail.com>
2023-12-14 20:20:59 -0500

    Updated Automatic Time Labels
    Time Labels are automatically linked to the OS time

--- scripts/emailTimeLabel.gd
+++ scripts/emailTimeLabel.gd
@@ -2,19 +2,26 @@ extends Label
 # Purpose of script is to ensure time is always up to date
 
 var time = OS.get_time()
-var time_return = String(time.hour) +":"+String(time.minute)
+var time_return: String
 
+var emailHour = time.hour - 1
+var emailMin = time.minute
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if(time.minute<10):
+	if(time.minute >10):
+		emailMin = (time.minute - 8)
+	else:
 		time_return = String(time.hour) +":"+"0"+String(time.minute)
+	time_return = String(emailHour) +":"+String(emailMin)
 	text = time_return
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if(time.minute<10):
+	if(time.minute >10):
+		emailMin = (time.minute - 8)
+	else:
 		time_return = String(time.hour) +":"+"0"+String(time.minute)
+	time_return = String(emailHour) +":"+String(emailMin)
 	text = time_return
-	#Time.get_time_string_from_system(false)
